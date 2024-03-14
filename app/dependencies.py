c9d30276f7559656047d643771c655439ab8170f
nikilase <niklas.eichenberg@tu-ilmenau.de>
2024-03-14 13:16:03 +0100

    Add bandit and black linter and refactor according to black

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,14 +1,17 @@
-from fastapi.templating import Jinja2Templates
 import tomllib
+
+from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="templates")
 
 try:
-	with open("app/conf/config.toml", "rb") as f:
-		config = tomllib.load(f)
+    with open("app/conf/config.toml", "rb") as f:
+        config = tomllib.load(f)
 except FileNotFoundError:
-	print("No custom config found!\nUsing template config!\nPlease configure your config in app/conf/config.toml")
-	with open("app/conf/config_template.toml", "rb") as f:
-		config = tomllib.load(f)
+    print(
+        "No custom config found!\nUsing template config!\nPlease configure your config in app/conf/config.toml"
+    )
+    with open("app/conf/config_template.toml", "rb") as f:
+        config = tomllib.load(f)
 
 server_config = config["server_config"]
--- app/main.py
+++ app/main.py
@@ -1,28 +1,25 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
-from app.dependencies import server_config
-from app.dependencies import templates
+from app.dependencies import server_config, templates
 
 app = FastAPI()
-app.add_middleware(
-	TrustedHostMiddleware, allowed_hosts=server_config["allowed_hosts"]
-)
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=server_config["allowed_hosts"])
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
 async def root(request: Request):
-	return templates.TemplateResponse("root.html", {"request": request})
+    return templates.TemplateResponse("root.html", {"request": request})
 
 
 @app.get("/favicon.ico")
 async def favicon():
-	return FileResponse("static/favicon/favicon.ico")
+    return FileResponse("static/favicon/favicon.ico")
 
 
 @app.get("/robots.txt")
 async def robots():
-	return FileResponse("static/robots.txt", media_type="text")
+    return FileResponse("static/robots.txt", media_type="text")
